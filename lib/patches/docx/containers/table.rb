a53ac6af45df20f2deef4928b81f80861eada9ab
Yuki Morohoshi <morohoshi@netlab.jp>
2016-03-08 00:05:29 +0900

    enable to add style to the table which have no style

--- lib/patches/docx/containers/table.rb
+++ lib/patches/docx/containers/table.rb
@@ -2,11 +2,22 @@ module Pandocxtyle
   module Patches
     refine ::Docx::Elements::Containers::Table do
       def style
-        @style ||= @node.xpath("w:tblPr/w:tblStyle/@w:val").first.value
+        if @node.xpath("w:tblPr/w:tblStyle/@w:val").first
+          @node.xpath("w:tblPr/w:tblStyle/@w:val").first.value
+        else
+          ""
+        end
       end
 
       def style=(new_style)
-        @node.xpath("w:tblPr/w:tblStyle/@w:val").first.value = new_style
+        if @node.xpath("w:tblPr/w:tblStyle/@w:val").first
+          @node.xpath("w:tblPr/w:tblStyle/@w:val").first.value = new_style
+        else
+          prop = @node.xpath("w:tblPr").first
+          style = Nokogiri::XML::Node::new("w:tblStyle", prop)
+          style["w:val"] = new_style
+          prop.add_child(style)
+        end
       end
     end
   end
